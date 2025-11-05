d1d4db7707619b86f5704c0f74da8a0ee38acc6d
ivanovichi <ivan.arthur18@gmail.com>
2025-11-05 09:05:20 -0600

    Code complete, but needs comments

--- BSTA.js
+++ BSTA.js
@@ -1,4 +1,264 @@
+class Node {
+    #coordinates; //Value of the node
+    #cor1;
+    #cor2;
+    #cor3;
+    #cor4;
+    #cor5;
+    #cor6;
+    #cor7;
+    #cor8;
+    #counter;
+    #father;
 
-function BSTAuxiliar () {
-    console.log("I am the auxuliar")
+    //Constructor
+    constructor (coordinates = 0, cor1 = null, cor2 = null, cor3 = null, cor4 = null, cor5 = null, cor6 = null, cor7 = null, cor8 = null, counter = 1, father = null) {
+        this.#coordinates = coordinates;
+        this.#cor1 = cor1;
+        this.#cor2 = cor2;
+        this.#cor3 = cor3;
+        this.#cor4 = cor4;
+        this.#cor5 = cor5;
+        this.#cor6 = cor6;
+        this.#cor7 = cor7;
+        this.#cor8 = cor8;
+        this.#counter = counter;
+        this.#father = father;
+    }
+
+    get getCoordinate() {
+        return this.#coordinates;
+    }
+
+    get getCor1() {
+        return this.#cor1;
+    }
+
+    get getCor2() {
+        return this.#cor2;
+    }
+
+    get getCor3() {
+        return this.#cor3;
+    }
+
+    get getCor4() {
+        return this.#cor4;
+    }
+
+    get getCor5() {
+        return this.#cor5;
+    }
+
+    get getCor6() {
+        return this.#cor6;
+    }
+
+    get getCor7() {
+        return this.#cor7;
+    }
+
+    get getCor8() {
+        return this.#cor8;
+    }
+
+    get getCounter() {
+        return this.#counter;
+    }
+
+    get getFather() {
+        return this.#father;
+    }
+
+    setCor1(value) {
+        this.#cor1 = value;
+    }
+
+    setCor2(value) {
+        this.#cor2 = value;
+    }
+
+    setCor3(value) {
+        this.#cor3 = value;
+    }
+
+    setCor4(value) {
+        this.#cor4 = value;
+    }
+
+    setCor5(value) {
+        this.#cor5 = value;
+    }
+
+    setCor6(value) {
+        this.#cor6 = value;
+    }
+
+    setCor7(value) {
+        this.#cor7 = value;
+    }
+
+    setCor8(value) {
+        this.#cor8 = value;
+    }
+
+    setCounter(value) {
+        this.#counter = value;
+    }
+
+    setValue(value) {
+        this.#coordinates = value;
+    }
+
+    setFather(value) {
+        this.#father = value;
+    }
 }
+
+class Tree {
+    #root;      //Root of the tree
+    #next;
+    #nextFather;
+    #levelOrder;
+    #nodesUse;
+
+    constructor(root = 0, next = 0, nextFather = 0, levelOrder = 0, nodesUse = 0) {
+        this.#root = root;
+        this.#next = next;
+        this.#nextFather = nextFather;
+        this.#levelOrder = levelOrder;
+        this.#nodesUse = nodesUse;
+    }
+
+    get getRoot() {
+        return this.#root;
+    }
+
+    get getLevelOrder() {
+        return this.#levelOrder;
+    }
+
+    //Functions that builds the tree
+    buildTree(array, root) {
+        let rootNode = new Node(root);
+        this.#root = rootNode;
+        for(let i = 0; i < array.length; i++) {
+            let leafNode = new Node(array[i]);
+            this.#root[`setCor${i + 1}`](leafNode);
+            leafNode.setFather(this.#root);
+        }
+        this.#nextFather = this.#root;
+        this.#next = this.#root.getCor1;
+        this.#levelOrder = this.levelOrder(this.#root, this.#nodesUse);
+        //Return the root
+        return this.#root;
+    }
+
+    //Function that inserts a new value
+    insert(coordinates, end) {
+        while(this.#next === null) { //IDEA CAMBIAR ESTE IF A UN WHILE
+            console.log("The next is null");
+            this.#levelOrder.splice(0,1);
+            this.#nodesUse += 1;
+            if(this.#levelOrder.length === 0) {
+                this.#levelOrder = this.levelOrder(this.#root, this.#nodesUse);
+                console.log("The new level order: ", this.#levelOrder);
+            }
+
+            this.#nextFather = this.#levelOrder[0];
+            console.log("The new value of the nextFather: ", this.#nextFather.getCoordinate);
+            this.#next = (this.#nextFather[`getCor${this.#nextFather.getCounter}`]);
+
+        }
+        let currentNode = this.#next;
+        console.log("The current Node: ", currentNode.getCoordinate);
+        console.log("The coordinates: ", coordinates, "Have a length of: ", coordinates.length);
+        for(let i = 0; i < coordinates.length; i++) {
+            let leafNode = new Node(coordinates[i]);
+            console.log("The leaf Node: ", leafNode.getCoordinate, "Is son of: ", currentNode.getCoordinate);
+            currentNode[`setCor${i + 1}`](leafNode);
+            leafNode.setFather(currentNode);
+            if(coordinates[i][0] === end[0] && coordinates[i][1] === end[1]) {
+                console.log("I found the coordinate");
+                return [true, leafNode];
+            }
+        }
+        this.#nextFather.setCounter(this.#nextFather.getCounter + 1);
+        console.log("The new counter of the nextFather is: ", this.#nextFather.getCounter);
+        if(this.#nextFather.getCounter > 8) {
+            console.log("The counter of nextFather is bigger than 8");
+            this.#next = null;
+        } else {
+            this.#next = (this.#nextFather[`getCor${this.#nextFather.getCounter}`]);
+            console.log("The new value of the next is: ", (this.#next === null) ? "null" : this.#next.getCoordinate);
+        }
+        return false;
+    }
+
+    levelOrder(node, useAlready) {
+        console.log("Level order start");
+        let theStorage = [];
+        //Create an array that will function like a queue
+        let theQueue = [];
+        //Start at the root
+        let temporalNode = node;
+        //Push the node in the array
+        theQueue.push(temporalNode);
+        //While loop that stops when the array is empty
+        while (theQueue.length !== 0) {
+            //Get the front element of the array
+            let theFront = theQueue.splice(0,1);
+            theStorage.push(theFront[0]);
+            console.log(theFront[0].getCoordinate);
+            //Temporal function is the value of the front of the list
+            temporalNode = theFront[0];
+            //If there is a node in the left
+            if(temporalNode.getCor1 != null) {
+                //Push the left child in the array
+                theQueue.push(temporalNode.getCor1);
+            }
+            if(temporalNode.getCor2 != null) {
+                //Push the left child in the array
+                theQueue.push(temporalNode.getCor2);
+            }
+            if(temporalNode.getCor3 != null) {
+                //Push the left child in the array
+                theQueue.push(temporalNode.getCor3);
+            }
+            if(temporalNode.getCor4 != null) {
+                //Push the left child in the array
+                theQueue.push(temporalNode.getCor4);
+            }
+            if(temporalNode.getCor5 != null) {
+                //Push the left child in the array
+                theQueue.push(temporalNode.getCor5);
+            }
+            if(temporalNode.getCor6 != null) {
+                //Push the left child in the array
+                theQueue.push(temporalNode.getCor6);
+            }
+            if(temporalNode.getCor7 != null) {
+                //Push the left child in the array
+                theQueue.push(temporalNode.getCor7);
+            }
+            if(temporalNode.getCor8 != null) {
+                //Push the left child in the array
+                theQueue.push(temporalNode.getCor8);
+            }
+        }
+        console.log("Level order finish");
+        if(useAlready > 0) {
+            theStorage.splice(0,useAlready);
+        }
+        return theStorage;
+    }
+}
+
+    // preOrderForEach(callback, node = this.#root) {
+    // }
+
+    // depth(value, node = this.#root, count = 0) {
+    // }
+
+
+export {Tree};
--- main.js
+++ main.js
@@ -1,58 +1,84 @@
 import {Tree} from "./BSTA.js";
 
+
 function knightMoves(begin, end) {
     if ((begin[0] < 0 || begin[0] > 7) || (begin[1] < 0 || begin[1] > 7) || (end[0] < 0 || end[0] > 7) || (end[1] < 0 || end[1] > 7)) {
         return "The coordinates are wrong, remember the grid is only 8 * 8 starting at zero";
     } else if (begin[0] === end[0] && begin[1] === end[1]) {
         return begin;
     }
-    let counter = 0;
     let theStopper = true;
     let theQueue = [];
     let theCorrectCor = 0;
     let theTraversal = [];
+    let alreadyUseMoves = [];
+    let repeatCoordinate = false;
+    let alreadyUseSon = [];
 
-    console.log(nextSteps(begin));
+    alreadyUseMoves.push(begin);
+    let possibleMovesArray = nextSteps(begin,alreadyUseMoves, alreadyUseSon);
 
-    let possibleMovesArray = nextSteps(begin);
+    for(let i = 0; i < possibleMovesArray.length; i++) {
+        if(end[0] === possibleMovesArray[i][0] && end[1] === possibleMovesArray[i][1]) {
+            return [begin,end];
+        }
+    }
     for (let i = 0; i < possibleMovesArray.length; i++) {
         theQueue.push(possibleMovesArray[i]);
     }
 
-    console.log(theQueue);
+    console.log("begin at: ", begin, "Next moves: ", possibleMovesArray);
 
     let stepsTree = new Tree();
     stepsTree.buildTree(possibleMovesArray, begin);
 
-    console.log(stepsTree.getRoot.getCoordinate);
-    for(let i = 1; i < 9; i++) {
-        console.log(stepsTree.getRoot[`getCor${i}`]);
-    }
-    console.log(stepsTree.getRoot.getCor1.getCoordinate);
-    console.log(stepsTree.getRoot.getCor2.getCoordinate);
+    // console.log(stepsTree.getRoot.getCoordinate);
+    // for(let i = 1; i < 9; i++) {
+    //     console.log(stepsTree.getRoot[`getCor${i}`]);
+    // }
+    // console.log(stepsTree.getRoot.getCor1.getCoordinate);
+    // console.log(stepsTree.getRoot.getCor2.getCoordinate);
+    // console.log("the queue: ", theQueue);
+    // console.log("Moves I already use: ", alreadyUseMoves);
+
+    console.log("The queue before starting: ", theQueue);
 
     while (theStopper ===  true) {
+        repeatCoordinate = false;
         let nextMove = theQueue.splice(0,1);
-        let newMoves = nextSteps(nextMove[0]);
+        for(let i = 0; i < alreadyUseMoves.length; i++) {
+            if(nextMove[0][0] === alreadyUseMoves[i][0] && nextMove[0][1] === alreadyUseMoves[i][1]) {
+                console.log("A repeat coordinate: ", nextMove, "In the array: ", alreadyUseMoves);
+                repeatCoordinate = true;
+            }
+        }
+        if (repeatCoordinate === false) {
+            let newMoves = nextSteps(nextMove[0],alreadyUseMoves, alreadyUseSon);
+            console.log("The current coordinate: ", nextMove, "Creates: ", newMoves);
+            for (let i = 0; i < newMoves.length; i++) {
+                theQueue.push(newMoves[i]);
+            }
 
-        console.log("new Moves: ", newMoves);
+            let finalFound = stepsTree.insert(newMoves, end);
+                if(finalFound[0]) {
+                theCorrectCor = finalFound[1];
+                console.log("The correct coordinate was: ", theCorrectCor);
+                theStopper = false;
+            }
+            alreadyUseMoves.push(nextMove[0]);
+            // console.log("End of the while, adding already use cor: ", alreadyUseMoves);
+            // console.log("The queue at the end fo the while: ", theQueue);
+        }
+    } 
 
-        let finalFound = stepsTree.insert(newMoves, end);
-        if(finalFound[0]) {
-            theCorrectCor = finalFound[1];
-            theStopper = false;
-        } else {
-        }       
-        counter += 1;
-        if(counter === 2) {
-            theStopper = false;
-        } 
-    }   
+    console.log("Final use of moves: ", alreadyUseMoves);
+    console.log("level order: ", stepsTree.getLevelOrder);
+
+    // let a = stepsTree.getRoot.getCor2;
+    // console.log(a.getCor2.getCoordinate);
 
     if(theCorrectCor !== 0) {
-
-        console.log("The correct coordinates: ", theCorrectCor.getCoordinate);
-
+        console.log("The path is found: ");
         theTraversal.unshift(theCorrectCor.getCoordinate);
         theCorrectCor = theCorrectCor.getFather;
         while(theCorrectCor !== null) {
@@ -69,7 +95,12 @@ function knightMoves(begin, end) {
 }
 
 //Function that gives the next possible steps
-function nextSteps(coordinates) {
+function nextSteps(coordinates, alreadyUse, alreadySon) {
+
+    let repeatCor = false;
+    let repeatSon = false;
+
+
     //Array that will store
     let totalMoves = [];
     //Set the values of the coordinate x and y
@@ -79,6 +110,7 @@ function nextSteps(coordinates) {
     for(let i = 1; i < 9; i++) {
         //Array that will store the coordinates of the possible square
         let individualMoves = [];
+
         //If i is prime change its value, every one step it will change value
         if(i % 2 != 0) {
             x *= -1;
@@ -87,8 +119,29 @@ function nextSteps(coordinates) {
         y *= -1;
         //Check if the coordinates are not lesser and bigger than the board (the board is 8 * 8)
         if((coordinates[0] + x > -1 && coordinates[1] + y > -1) && (coordinates[0] + x <= 7 && coordinates[1] + y <= 7)) {
+
+            repeatCor = false;
+            repeatSon = false;
             individualMoves.push(coordinates[0] + x, coordinates[1] + y);
-            totalMoves.push(individualMoves);
+
+            for(let i = 0; i < alreadyUse.length; i++) {
+                if(individualMoves[0] === alreadyUse[i][0] && individualMoves[1] === alreadyUse[i][1]) {
+                    repeatCor = true;
+                }
+            }
+
+            for(let i = 0; i < alreadySon.length; i++) {
+                if(individualMoves[0] === alreadySon[i][0] && individualMoves[1] === alreadySon[i][1]) {
+                    repeatSon = true;
+                }
+            }
+
+
+            if (repeatCor === false && repeatSon === false) {
+                alreadySon.push(individualMoves);
+                totalMoves.push(individualMoves);
+            }
+
         }
         //If i is 4 change the values of x and y
         if(i === 4){
@@ -96,16 +149,8 @@ function nextSteps(coordinates) {
             y = 1;
         }
     }
+
     return totalMoves;
 }
 
-console.log(knightMoves([0,0],[0,0])); 
-
-
-//COSAS POR ARREGLAR:
-//CHECAR CUANDO INICIO Y FINAL SEAN IGUALES: LISTO
-//CHECAR QUE INICIO Y FINAL SEAN COORDENADAS VALIDAS: LISTO 
-//HACER QUE EL WHILE DEL QUEUE SIGA POR MAS DE DOS VECES
-//HACER QUE EL NEXT Y NEXTFATHER SE MUEVAN PARA MAS RAMAS
-//CHECAR QUE NO HAYA MAS ERRORES
-//COMENTAR
+console.log(knightMoves([0,0],[7,7])); 
